48b3bc4273d7a9b4be81cc219d2cd79f553fe6e7
Dominik Menke <dom@digineo.de>
2021-09-23 11:25:05 +0200

    dev: change build fragment permissions in build_static.sh
    The static build script build postgres_exporter statically in
    a container, with the current working directory mounted inside.
    
    By default, the container's user is root, and in effect the
    build fragment is not owned by the user running the script.
    
    This just changes the owner of the output file.

--- build_static.sh
+++ build_static.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-docker run -i -v `pwd`:/postfix_exporter golang:1 /bin/sh << 'EOF'
-set -ex
+uid=$(id -u)
+gid=$(id -g)
 
+docker run -i -v "$(pwd):/postfix_exporter" golang:1 /bin/sh -ex <<EOF
 # Install prerequisites for the build process.
 apt-get update -q
 apt-get install -yq libsystemd-dev
 
 cd /postfix_exporter
 
-go get -d ./...
-go build -a -tags static_all
-strip postfix_exporter
+go mod download
+go build -a -tags static_all -ldflags="-s -w"
+chown $uid:$gid ./postfix_exporter
 EOF
